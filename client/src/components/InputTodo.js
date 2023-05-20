225f2d50834c9bb9aa29d0bccbe3f0ee0edb6bf0
mike-ninja <mikeedubas@hotmaiL.com>
2023-05-20 10:17:38 +0300

    input form now re-renders after submission

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,39 +1,47 @@
-import { Fragment, useState } from 'react'
-import todoServices from '../services/todo'
+import { Fragment, useState } from 'react';
+import todoServices from '../services/todo';
 
 const InputTodo = () => {
-  const [task, setTask] = useState('')
+  const [task, setTask] = useState('');
 
   const inputHandler = (event) => {
-    setTask(event.target.value)
-  }
+    setTask(event.target.value);
+  };
 
   const onSubmitForm = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
-      const currentDate = new Date().toISOString()
+      const currentDate = new Date().toISOString();
       const newTodo = {
         task: task,
         status: false,
-        due_date: currentDate
-      }
-      console.log(newTodo)
-      todoServices
-        .createTodo(newTodo)
+        due_date: currentDate,
+      };
+      console.log(task);
+      todoServices.createTodo(newTodo).then((createdTodo) => {
+        console.log(createdTodo);
+        setTask((prevTask) => '');
+      });
     } catch (error) {
-      console.log(error.message) 
+      console.log(error.message);
     }
-  }
+  };
 
   return (
     <Fragment>
       <h2 className="text-center my-5">Input Todo</h2>
       <form className="d-flex" onSubmit={onSubmitForm}>
-        <input type="text" placeholder="add task" className="form-control" onChange={inputHandler}/>
+        <input
+          type="text"
+          placeholder="add task"
+          className="form-control"
+          value={task}
+          onChange={inputHandler}
+        />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
-  )
-}
+  );
+};
 
-export default InputTodo
+export default InputTodo;
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,35 +1,31 @@
-import { Fragment, useEffect, useState } from 'react'
-import EditTodo from './EditTodo'
-import todoService from '../services/todo'
+import { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo';
+import todoService from '../services/todo';
 
 const ListTodos = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   async function deleteTodo(id) {
     try {
-      todoService
-        .deleteTodo(id)
-      setTodos(todos.filter(todo => todo.id !== id))
+      await todoService.deleteTodo(id); // Wait for deletion to complete
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
     } catch (error) {
-      console.log(`Failed to delete todo: ${error}`)
+      console.log(`Failed to delete todo: ${error}`);
     }
   }
 
   async function getTodos() {
     try {
-      todoService
-        .getAllTodo()
-        .then(todoArray => {
-          setTodos(todoArray)
-        })
+      const todoArray = await todoService.getAllTodo(); // Wait for todos to be fetched
+      setTodos(todoArray);
     } catch (error) {
-      console.log(`Failed to fetch todos: ${error}`)
+      console.log(`Failed to fetch todos: ${error}`);
     }
   }
 
   useEffect(() => {
-    getTodos()
-  }, [])
+    getTodos();
+  }, []);
 
   return (
     <Fragment>
@@ -42,25 +38,27 @@ const ListTodos = () => {
           </tr>
         </thead>
         <tbody>
-          {todos.map(todo => {
+          {todos.map((todo) => {
             return (
               <tr key={todo.id}>
                 <td>{todo.task}</td>
                 <td>{todo.status ? 'Completed' : 'Ongoing'}</td>
                 <td>{todo.due_date}</td>
-                <td><EditTodo todo={todo}/></td>
+                <td>
+                  <EditTodo todo={todo} />
+                </td>
                 <td>
                   <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>
                     Delete
                   </button>
                 </td>
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
     </Fragment>
-  )
-}
+  );
+};
 
-export default ListTodos
+export default ListTodos;
