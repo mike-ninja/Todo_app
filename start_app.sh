edc3af42d0778faf25ab03bde93cab01825c4bc8
mike-ninja <mikeedubas@hotmaiL.com>
2023-05-23 18:10:30 +0300

    minor change for start_app script

--- start_app.sh
+++ start_app.sh
@@ -3,6 +3,6 @@
 # Build directory in the client directory and copy to the server directory
 cd client && npm run build && cp -r ./build ../server/build && cd ..
 
-cd server && npm run server
+cd server && npm run start
 
 
